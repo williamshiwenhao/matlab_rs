f80d57b0e9de7adb3bf7d5da3b491957888f5993
william <987700896@qq.com>
2019-04-16 18:08:28 +0800

    class

--- RsDecodeIterate.m
+++ RsDecodeIterate.m
@@ -1,21 +1,21 @@
 function [errPosPolyCalc, sigma  ] = RsDecodeIterate( S )
 %RsDecodeIterate
 batch = size(S, 1);
-sigma = zeros(GfTable.r+1, GfTable.r+2);
+sigma = zeros(GfTable.r+2, GfTable.r+1);
 sigma(1,1)=1;
 sigma(2,1)=1;
 d = zeros(batch, GfTable.r+2);
-di = 1;
-d(:,1) = S(:,1);
+d(:,1) = 1;
+d(:,2) = S(:,1);
 D = zeros(batch , GfTable.r+2);
 D(:, 1) = 0;    %D(-1)  = 0
 D(:, 2) = 0;    %D(0)   = 0
 ii = -1;
 
 %index
-%paper: -1 0 1 2 ...
-%jj:     1 2 3 4 ...
-%ii:     1 2 3 4
+%paper: -1 0 1 2 ...r-1
+%jj:     1 2 3 4 ...r+1
+%ii:     1 2 3 4 ...r+1
 for jj = 2:GfTable.r+1
 
     if d(:,jj)==0
@@ -39,26 +39,28 @@ for jj = 2:GfTable.r+1
         % Cal sigma
         dMul = RsSymbolMul(d(jj), RsSymbolRev(d(ii)));
         rPoly = zeros(1, GfTable.r+1);
-        rPoly(jj - ii + 1) = dMul;
-        sigma(jj+1, :) = rPoly;
+        rPoly(jj - ii+1) = dMul;
+        sigma(jj+1, :) = RsPolySub(sigma(jj, :), rPoly);
         % Get D(jj+1)
         for kk = GfTable.r+1 : -1 : 1
             if sigma(jj+1, kk) ~= 0
-                D(jj+1) = kk - 1;
+                D(jj+1) = kk -1
                 break;
             end
         end
     end
-    if jj == GfTable.r
+    if jj == GfTable.r+1
         break;
     end
     % Get next d
     d(:,jj+1) = S(jj);
-    for kk = 1:D(jj+1)
-        d(:,jj+1)=RsSymbolAdd(d(:,jj+1) , RsSymbolMul(S(jj+1-kk), sigma(kk+1, jj+1)));
+    for kk = 1:jj-1
+        d(:,jj+1)=RsSymbolAdd(d(:,jj+1) , RsSymbolMul(S(jj-kk), sigma(jj+1, kk)));
     end
+    sigma
 end
 errPosPolyCalc = sigma(end, :);
 sigma
+d
 end
 
