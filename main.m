d163d16e002a4b3e921bd8e800accfc3b228642a
william <987700896@qq.com>
2019-04-17 22:29:55 +0800

    fix some bug

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ r = GfTable.k / GfTable.n;
 snr = @(ebn0)ebn0 + 10*log10(r / 0.5);
 
 earlyStop = 100;
-enb0 = 0:1:8;
+ebn0 = 0:1:8;
 errorSymbolRate = zeros(1, length(ebn0));
 errorFrameRate = zeros(1, length(ebn0));
 batchSize = 100;
@@ -13,20 +13,20 @@ for ii = 1 : length(ebn0)
     sumSymbol = 0;
     sumFrame = 0;
     for jj = 1 : totalCycle / batchSize
-        [symbol, frame] = RsSendAndReceive(snr(enb0(ii)), batchSize);
+        [symbol, frame] = RsSendAndReceive(snr(ebn0(ii)), batchSize);
         sumSymbol = sumSymbol + symbol;
         sumFrame = sumFrame + frame;
         if sumSymbol > earlyStop
             break;
         end 
     end
-    errorSymbolRate(ii) = sumSymbol / (jj * batchSize);
-    errorFrameRate(jj) = sumFrame / (jj * batchSize);
+    errorSymbolRate(ii) = sumSymbol / (jj * batchSize * GfTable.k)
+    errorFrameRate(ii) = sumFrame / (jj * batchSize)
 end
 save('first time.mat')
 semilogy(ebn0, errorSymbolRate);
 hold on;
 semilogy(ebn0, errorFrameRate);
     
-end
+
 
